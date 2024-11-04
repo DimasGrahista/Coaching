4983934ce19274ca19fea97005d2b0446f63a090
DimasGrahista <dimasgrahista@gmail.com>
2024-11-04 16:37:45 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,264 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* nav */}
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#!">Dinatt Games</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#!">Coaching</a></li>
+                                <li><hr class="dropdown-divider" /></li>
+                                <li><a class="dropdown-item" href="#!">TopUp</a></li>
+                                <li><a class="dropdown-item" href="#!">Boost Rank</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <button class="btn btn-outline-dark" type="submit">
+                            <i class="bi-cart-fill me-1"></i>
+                            Cart
+                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        {/* header */}
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder"><tag>Easy To Learn <s className="text-muted text-decoration-line-through">hard</s> Easy To Master</tag></h1>
+                    <p class="lead fw-normal text-white-50 mb-0">Kalian Malu Rank Kalian Ga Naik-Naik? Cukup Siji Solusinya Pergi Ke Dinatt Saja Langung Sambat Mas Saya Butuh Coaching, Kalau Tidak Berhasil Pake Jurus Yang Kedua Minta Tolong Namanya Ngejoki Contohnya</p>
+                </div>
+            </div>
+        </header>
+        {/* Section*/}
+      <section className="py-5">
+        <div className="container px-4 px-lg-5 mt-5">
+        <h1>Coaching</h1>
+          <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+            <div className="col mb-5">
+              <div className="card h-100">
+                {/* Product image*/}
+                <img className="card-img-top" src="./img/valorant.png" alt="..." />
+                {/* Product details*/}
+                <div className="card-body p-4">
+                <div className="badge bg-dark text-white position-absolute" style={{top: '0.5rem', right: '0.5rem'}}>Trending</div>
+                  <div className="text-center">
+                    {/* Product name*/}
+                    <h5 className="fw-bolder">Valorant</h5>
+                    {/* Product reviews*/}
+                    <div className="d-flex justify-content-center small text-warning mb-2">
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                    </div>
+                    {/* Product price*/}
+                    Rp150.000 - Rp300.000
+                  </div>
+                </div>
+                {/* Product actions*/}
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                {/* Sale badge*/}
+                <div className="badge bg-dark text-white position-absolute" style={{top: '0.5rem', right: '0.5rem'}}>Tranding</div>
+                {/* Product image*/}
+                <img className="card-img-top" src="./img/rsix.png" alt="..." />
+                {/* Product details*/}
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    {/* Product name*/}
+                    <h5 className="fw-bolder">Rainbow Six</h5>
+                    {/* Product reviews*/}
+                    <div className="d-flex justify-content-center small text-warning mb-2">
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                    </div>
+                    {/* Product price*/}
+                    <span className="text-muted text-decoration-line-through">Rp30.000</span>
+                    Rp147.000 - Rp290.000
+                  </div>
+                </div>
+                {/* Product actions*/}
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                {/* Sale badge*/}
+                <div className="badge bg-dark text-white position-absolute" style={{top: '0.5rem', right: '0.5rem'}}>Tranding</div>
+                {/* Product image*/}
+                <img className="card-img-top" src="./img/apex-01.png" alt="..." />
+                {/* Product details*/}
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    {/* Product name*/}
+                    <h5 className="fw-bolder">APEX</h5>
+                    {/* Product price*/}
+                    <span className="text-muted text-decoration-line-through">Rp50.000</span>
+                    Rp150.000-Rp500.000
+                  </div>
+                </div>
+                {/* Product actions*/}
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                {/* Product image*/}
+                <img className="card-img-top" src="./img/cs2.png" alt="..." />
+                {/* Product details*/}
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    {/* Product name*/}
+                    <h5 className="fw-bolder">Counter Strike 2</h5>
+                    {/* Product reviews*/}
+                    <div className="d-flex justify-content-center small text-warning mb-2">
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                    </div>
+                    {/* Product price*/}
+                    $40.00
+                  </div>
+                </div>
+                {/* Product actions*/}
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                {/* Sale badge*/}
+                <div className="badge bg-dark text-white position-absolute" style={{top: '0.5rem', right: '0.5rem'}}>Sale</div>
+                {/* Product image*/}
+                <img className="card-img-top" src="./img/pubg.png" alt="..." />
+                {/* Product details*/}
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    {/* Product name*/}
+                    <h5 className="fw-bolder">Player Unknow Battle Ground</h5>
+                    {/* Product price*/}
+                    <span className="text-muted text-decoration-line-through">Rp50.000</span>
+                    Rp299.000
+                  </div>
+                </div>
+                {/* Product actions*/}
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                {/* Product image*/}
+                <img className="card-img-top" src="./img/dl.png" alt="..." />
+                {/* Product details*/}
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    {/* Product name*/}
+                    <h5 className="fw-bolder">Dead Lock</h5>
+                    {/* Product price*/}
+                    Rp200.000
+                  </div>
+                </div>
+                {/* Product actions*/}
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                {/* Sale badge*/}
+                {/* <div className="badge bg-dark text-white position-absolute" style={{top: '0.5rem', right: '0.5rem'}}>Sale</div> */}
+                {/* Product image*/}
+                <img className="card-img-top" src="./img/thesims.png" alt="..." />
+                {/* Product details*/}
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    {/* Product name*/}
+                    <h5 className="fw-bolder">The Sims</h5>
+                    {/* Product reviews*/}
+                    <div className="d-flex justify-content-center small text-warning mb-2">
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                    </div>
+                    {/* Product price*/}
+                    <span className="text-muted text-decoration-line-through">Rp21.000</span>
+                    Rp99.000
+                  </div>
+                </div>
+                {/* Product actions*/}
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                {/* Product image*/}
+                <img className="card-img-top" src="./img/lol.png" alt="..." />
+                {/* Product details*/}
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    {/* Product name*/}
+                    <h5 className="fw-bolder">league Of legends</h5>
+                    {/* Product reviews*/}
+                    <div className="d-flex justify-content-center small text-warning mb-2">
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                      <div className="bi-star-fill" />
+                    </div>
+                    {/* Product price*/}
+                    Rp200.000 - Rp600.000
+                  </div>
+                </div>
+                {/* Product actions*/}
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+{/* Footer*/}
+
+    </>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,6 @@
+function Abput() {
+    return <h1>Ini Halaman About</h1>;
+  }
+  
+  export default Abput;
+  
